482d7dbd7283fc3577756bf2cf3b6a0124e58e6b
umarshaikh2000 <umar.shaikh02@outlook.com>
2023-12-14 21:59:54 +0530

    currency changed

--- frontend/src/components/Price/Price.js
+++ frontend/src/components/Price/Price.js
@@ -1,3 +1,19 @@
+// import React from 'react';
+
+// export default function Price({ price, locale, currency }) {
+//   const formatPrice = () =>
+//     new Intl.NumberFormat(locale, {
+//       style: 'currency',
+//       currency,
+//     }).format(price);
+
+//   return <span>{formatPrice()}</span>;
+// }
+
+// Price.defaultProps = {
+//   locale: 'en-US',
+//   currency: 'USD',
+// };
 import React from 'react';
 
 export default function Price({ price, locale, currency }) {
@@ -11,6 +27,6 @@ export default function Price({ price, locale, currency }) {
 }
 
 Price.defaultProps = {
-  locale: 'en-US',
-  currency: 'USD',
+  locale: 'en-IN', // Change locale to 'en-IN' for Indian English
+  currency: 'INR', // Change currency to 'INR' for Indian Rupees
 };
